5ba638ce9a5f1fb1aeb731361d6b3aff012c7669
Ivan <ivzak@yandex.ru>
2018-09-15 22:51:33 +0300

    conf.py

--- conf.py
+++ conf.py
@@ -216,7 +216,11 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.*', 'README.rst']
+exclude_patterns = [
+    '.*',
+    'README.rst',
+    'ru/medplus.rst'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
